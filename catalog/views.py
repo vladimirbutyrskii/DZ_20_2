16c6db26f08581b095a8c0eb2a5ae25284d48b5a
vladimirbutyrskii <vlaver@mail.ru>
2024-08-21 00:15:45 +0300

    комит detail

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'base.html')
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'products_list.html', context)
+
+
+def products_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'products_detail.html', context)
